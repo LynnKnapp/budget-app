a008f341a5b6bdcd191ef75e72d5b36bdf5b60e3
Lynn Knapp <ebonyeek@gmail.com>
2020-02-19 23:29:24 -0500

    App complete, working on expenseList

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import Alert from './components/Alert';
-import ExpenseList from'./components/ExpenseList'
+import React, {useState} from 'react'
+import Alert from './components/Alert'
+import ExpenseList from'./components/ExpenseList.js'
+import ExpenseForm from'./components/ExpenseForm.js'
 import './styles/styles.css'
+import uuid from 'uuid/v4'
 
-import React from 'react'
+const initialExpenses = [
+  {id: uuid(), charge:'mortgage', amount: 1250},
+  {id: uuid(), charge:'auto payment', amount: 450},
+  {id: uuid(), charge:'insurance', amount: 500},
+  {id: uuid(), charge:'credit cards', amount: 1000},
 
-export const App = () => {
+]
+
+function App () {
+  
+  const [expenses, setExpenses] = useState(initialExpenses)
+  
+  
+  
   return (
-    <div>
-      
-    </div>
+    <>
+      <Alert />
+      <h1>Budget Calculator</h1>
+      <div className='App'>
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </div>
+      <h1>
+        Total Spending: 
+        <span className='total'>
+         $ {expenses.reduce((acc, curr) =>{
+           return (acc += curr.amount)
+         },0)}
+        </span>
+      </h1>
+    </>
   )
 }
 
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Expense from './Expense'
+import {MdDelete} from 'react-icons/md'
 
-export const ExpenseList = () => {
+
+const ExpenseList = (expenses) => {
     return (
-        <div>
-            List
-            <Expense />
-        </div>
+        <>
+            <ul className='list'>
+            {expenses.map((expense) =>{
+             return <Expense key={expense.id} expense={expense} />  
+            })}
+            </ul> 
+            {expenses.length > 0 && <button className='btn'>
+                Clear Expenses
+                <MdDelete className='btn-icon' />   
+            </button>}
+
+        </>
     )
 }
 
